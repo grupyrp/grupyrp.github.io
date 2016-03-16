d32a90cdc88b23aaaf969bf631f2349a276106ea
Danilo Shiga <daniloshiga@gmail.com>
2016-03-16 16:54:21 -0300

    Updating the members.py plugin

--- plugins/members/members.py
+++ plugins/members/members.py
@@ -2,16 +2,17 @@
 Members info plugin for Pelican
 ===============================
 
-This plugin replaces the ``members`` variable with a structure of it's contents
-for use in the template
+This plugin looks for a ``members`` metadata header containing key/value pairs
+and makes them available for use in templates
 
-it takes a :members: metadata where the first line defines each key for the
-final dict, and the following lines have each member info
+The first line of the members metadata defines each key, and the following
+lines contain corresponding values for each member.
 
 :members: nome, email, twitter, github, site_nome, site_href
     Danilo Shiga, daniloshiga@gmail.com, @daneoshiga, daneoshiga, Danilo Shiga, http://daniloshiga.com
 """
 
+from collections import OrderedDict
 
 from pelican import signals
 
@@ -19,11 +20,16 @@ from pelican import signals
 def add_members(generator, metadata):
 
     if 'members' in metadata.keys():
-        members = metadata['members'].encode('utf-8').splitlines()
-        metadata['members'] = dict()
-        keys = map(str.strip, members[0].split(','))
+        # Dealing with differences on metadata for md and rst content
+        if type(metadata['members']) == list:
+            members = metadata['members']
+        else:
+            members = metadata['members'].splitlines()
+
+        metadata['members'] = OrderedDict()
+        keys = map(unicode.strip, members[0].split(','))
         for member in members[1:]:
-            values = map(str.strip, member.split(','))
+            values = map(unicode.strip, member.split(','))
             member_dict = dict(zip(keys, values))
             metadata['members'][member_dict['nome']] = member_dict
 
