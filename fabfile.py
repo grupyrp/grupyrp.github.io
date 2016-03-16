b2f303c4d25922c14668fd9993dd19c38db9b4c5
Danilo Shiga <daniloshiga@gmail.com>
2016-03-16 17:18:10 -0300

    Updating branch for github page, since this is an organization page, use master

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,7 @@ env.cloudfiles_api_key = 'my_rackspace_api_key'
 env.cloudfiles_container = 'my_cloudfiles_container'
 
 # Github Pages configuration
-env.github_pages_branch = "gh-pages"
+env.github_pages_branch = "master"
 
 # Port for `serve`
 PORT = 8000
