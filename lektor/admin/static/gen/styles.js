4e12eca86f47205be3aedb5392977dac30defa64
jucarmona <juhhcarmona@gmail.com>
2016-10-27 22:03:29 -0200

    Adding Lektor basic setup

---
+++ lektor/admin/static/gen/styles.js
@@ -0,0 +1,8 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("// removed by extract-text-webpack-plugin\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2xlc3MvbWFpbi5sZXNzP2FkODEiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL2xlc3MvbWFpbi5sZXNzXG4vLyBtb2R1bGUgaWQgPSAwXG4vLyBtb2R1bGUgY2h1bmtzID0gMSJdLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }
+]);
